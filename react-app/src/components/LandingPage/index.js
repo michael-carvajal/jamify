2fd18a48ab7393f0ca773e7eaeb7655767f8665f
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-16 11:41:51 -0400

    landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,11 +1,23 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-
+import "./landingpage.css"
 export default function LandingPage({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
+    const { songsheets } = useSelector(state => state)
+    const { Songsheets, Artists, Albums } = songsheets
+    if (!Songsheets) {
+        // Show a loading screen or spinner while the data is being fetched
+        return <div>Loading...</div>
+    }
+     const songsheetsArray = Object.values(Songsheets)
+
+
     return (
         <div className="landing-page">
-            <h1>landing page</h1>
+            <h1>Welcome to Jamify</h1>
+            <div className="recently-published">
+
+            </div>
         </div>
     )
 }
