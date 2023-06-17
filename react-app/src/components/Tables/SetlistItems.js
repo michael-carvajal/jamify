6aff68e8696c84d2c4fdb062befbfe9d37e4b877
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-17 12:45:20 -0400

    able to display songs in a setlist

--- react-app/src/components/Tables/SetlistItems.js
+++ react-app/src/components/Tables/SetlistItems.js
@@ -6,25 +6,46 @@ export default function SetlistItems() {
     const { setlists } = useSelector(state => state)
     const { songsheets } = useSelector(state => state)
     const { Setlists, Setlist_items } = setlists;
-    const {Songsheets} = songsheets
-    if (!Setlists) {
+    const {Songsheets, Artists, Genres} = songsheets
+    if (!Setlists || !Songsheets) {
         // Show a loading screen or spinner while the data is being fetched
         return <div>Loading...</div>;
     }
     const setlist = Setlists[listId]
     const listItems = Object.values(Setlist_items);
     const items = listItems.filter(list => list.setlist_id === parseInt(listId))
-    const songs = items.filter(item => {
-        console.log(item);
-        console.log(Songsheets[item.songsheet_id]);
-        return item
+    const songs = items.map(item => {
+        return {
+            song: Songsheets[item.songsheet_id],
+            artist : Artists[Songsheets[item.songsheet_id].artist_id],
+            genre : Genres[Songsheets[item.songsheet_id].genre_id],
+        }
     })
 
     console.log(songs);
     return (
         <div>
-            Setlist details
+            <table>
+                <thead>
+                    <tr>
+                        <th>ARTIST</th>
+                        <th>SONG</th>
+                        <th>GENRE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {songs.map((song, index) => {
+                        return (
+                            <tr key={`list-item-list-${index}`}>
+                                <th>{song.artist.name}</th>
+                                <th>{song.song.title}</th>
+                                <th>{song.genre.name}</th>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 
 }
