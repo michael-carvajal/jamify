55c25623b723b4d06d30f427db31ed1af0a4cbbf
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-21 14:09:36 -0400

    number setlists a a song has

--- react-app/src/components/Songsheets/SongsheetDetail.js
+++ react-app/src/components/Songsheets/SongsheetDetail.js
@@ -5,19 +5,21 @@ import OpenModalButton from "../OpenModalButton"
 import AllSetlist from "../Tables/Setlists"
 export default function SongsheetDetail() {
     const { songsheets } = useSelector(state => state)
+    const { setlists } = useSelector(state => state)
     const { songId } = useParams()
     const { Songsheets, Artists } = songsheets
-    if (!Songsheets) {
+    if (!Songsheets || !setlists) {
         return <div>Loading...</div>
     }
     // const songsheetsArray = Object.values(Songsheets)
     const songsheet = Songsheets[songId]
     const artist = Artists[songsheet.artist_id]
-    // console.log("this is the bdy  =========>", songsheet.body);
+    const songSetlists = Object.values(setlists?.Setlist_items).filter(list => list.songsheet_id === parseInt(songId) )
+    console.log("this is the bdy  =========>", songSetlists);
     return (
         <div id="songsheet-detail">
             <h1>{songsheet.title} by {artist.name}</h1>
-            <p>Added to 15 Setlists</p>
+            <p>Added to {songSetlists.length} Setlists</p>
             <OpenModalButton type="add-to-setlist" modalComponent={<AllSetlist type="add" songId={songsheet.id} />} />
             <p>Key {songsheet.key}</p>
             <p>Author (have to find the author) last edit on {songsheet.updated_at}</p>
