3374015a316ec0ae574f988bd94fa43c1b8d92a6
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-24 11:17:56 -0400

    maybe just maybe

--- app/seeds/songsheets.py
+++ app/seeds/songsheets.py
@@ -136,7 +136,7 @@ def seed_songsheets():
 
 
     if(os.environ.get("FLASK_ENV") == "production"):
-        chrome_options.binary_location = '/opt/render/project/.render/chrome/chromedriver'  # Update with the correct Chrome binary location in WSL
+        chrome_options.binary_location = '/opt/render/project/.render/chrome/chromedriver/google-chrome-stable'  # Update with the correct Chrome binary location in WSL
     else:
         chrome_options.binary_location = '/usr/bin/google-chrome-stable'  # Update with the correct Chrome binary location in WSL
 
