953b942921843b66734e36a2aa8a150210cf9423
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-28 11:25:24 -0400

    pianooooooooooooo

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ import PublishSongsheet from "./components/Songsheets/PublishSongsheet";
 import AllSetlist from "./components/Tables/Setlists";
 import SetlistItems from "./components/Tables/SetlistItems";
 import UserSetlists from "./components/User/UserSetlists";
+import Keyboard from "./components/Keyboard";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ function App() {
 
   return (
     <>
+      <Keyboard />
       <div class="custom-shape-divider-top-1687453054">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
