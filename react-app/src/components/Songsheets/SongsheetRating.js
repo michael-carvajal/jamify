e2191a30cedc88c69df762cdd09e3471a8f499d6
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-07-11 14:25:59 -0400

    reversed order of reviews

--- react-app/src/components/Songsheets/SongsheetRating.js
+++ react-app/src/components/Songsheets/SongsheetRating.js
@@ -51,7 +51,7 @@ export default function SongsheetRatings() {
                             )}
                         </div>
                     );
-                })}
+                }).reverse()}
             </div>
         </>
     );
