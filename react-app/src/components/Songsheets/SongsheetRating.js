ba6d190fc42c3f42b5d506bb5d30c2a4e8029787
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-07-11 16:26:57 -0400

    starting to edit

--- react-app/src/components/Songsheets/SongsheetRating.js
+++ react-app/src/components/Songsheets/SongsheetRating.js
@@ -5,13 +5,15 @@ import OpenModalButton from "../OpenModalButton";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import DeleteRatingModal from "../Reviews/DeleteRatingModal";
 import ReviewForm from "../Reviews/ReviewForm";
+
 export default function SongsheetRatings() {
     const { session } = useSelector(state => state);
     const { ratings } = useSelector(state => state);
-    // const { songsheets } = useSelector(state => state);
     const dispatch = useDispatch();
-    const { songId } = useParams()
+    const { songId } = useParams();
     const [isLoading, setIsLoading] = useState(true);
+    const [editRatingId, setEditRatingId] = useState(null);
+
     useEffect(() => {
         const fetchData = async () => {
             await dispatch(fetchRatings());
@@ -23,32 +25,67 @@ export default function SongsheetRatings() {
     if (isLoading || !ratings) {
         return <h2>Loading...</h2>;
     }
-    const songsheetRatings = Object.values(ratings).filter(rating => rating.songsheet_id === parseInt(songId));
-    const isReviewed = songsheetRatings.find(rating => rating.author_id === session.user?.id)
-    console.log(isReviewed);
+
+    const songsheetRatings = Object.values(ratings).filter(
+        rating => rating.songsheet_id === parseInt(songId)
+    );
+    const isReviewed = songsheetRatings.find(
+        rating => rating.author_id === session.user?.id
+    );
+
+    const handleEditRating = (ratingId) => {
+        setEditRatingId(ratingId);
+    };
+
+    const handleCancelEdit = () => {
+        setEditRatingId(null);
+    };
+
     return (
         <>
             <h1>Songsheet Ratings</h1>
-            {session.user && ((!isReviewed) && <ReviewForm songsheet_id={parseInt(songId)} author_id={session.user?.id}/>)}
+            {session.user && !isReviewed && (
+                <ReviewForm songsheet_id={parseInt(songId)} author_id={session.user?.id} />
+            )}
             <div className="user-ratings">
                 {songsheetRatings.map((rating, ind) => {
-                    const author = session.allUsers.users.find(user => rating.author_id === user.id)
-                    // console.log(author);
+                    const author = session.allUsers.users.find(
+                        user => rating.author_id === user.id
+                    );
+                    const isEditing = rating.id === editRatingId;
+
                     return (
                         <div key={`songsheet-rating-idx-${ind}`} className="user-rate">
-                            <p>{rating.comment}</p>
-                            <p>{rating.rating}<i className="fa fa-star"></i></p>
-                            <p>Written by: {author.username}</p>
+                            {isEditing ? (
+                                <ReviewForm
+                                    songsheet_id={parseInt(songId)}
+                                    author_id={session.user?.id}
+                                    editRating={rating.rating}
+                                    editComment={rating.comment}
+                                    onCancel={handleCancelEdit}
+                                />
+                            ) : (
+                                <>
+                                    <p>{rating.comment}</p>
+                                    <p>
+                                        {rating.rating}
+                                        <i className="fa fa-star"></i>
+                                    </p>
+                                    <p>Written by: {author.username}</p>
 
-                            {session.user?.id === rating.author_id && (
-                                <div className="delete-edit">
-                                    <button>Edit</button>
+                                    {session.user?.id === rating.author_id && (
+                                        <div className="delete-edit">
+                                            <button onClick={() => handleEditRating(rating.id)}>
+                                                Edit
+                                            </button>
 
-                                    <OpenModalButton
-                                        type="delete-rating"
-                                        modalComponent={<DeleteRatingModal ratingId={rating.id} />}
-                                    />
-                                </div>
+                                            <OpenModalButton
+                                                type="delete-rating"
+                                                modalComponent={<DeleteRatingModal ratingId={rating.id} />}
+                                            />
+                                        </div>
+                                    )}
+                                </>
                             )}
                         </div>
                     );
