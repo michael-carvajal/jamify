cdb15add885a04768951728c5063803c1ca51452
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-07-13 20:46:23 -0400

    completed demo upload

--- react-app/src/components/Demos/index.js
+++ react-app/src/components/Demos/index.js
@@ -40,18 +40,30 @@ const Demos = () => {
         setAudioFile(file);
     };
 
-    const handleDemoUpload = () => {
-        const blob = fetch(jammin)
-            .then((response) => response.blob())
-            .then((blob) => {
-                const file = new File([blob], "jammin.mp3", { type: "audio/mp3" });
-                setAudioFile(file);
-                setName('Jammin by Bob Marley')
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const handleDemoUpload = async () => {
+        try {
+            const response = await fetch(jammin);
+            const blob = await response.blob();
+            const file = new File([blob], "jammin.mp3", { type: "audio/mp3" });
+            setAudioFile(file);
+            const demoName = "Jammin by Bob Marley";
+
+            const formData = new FormData();
+            formData.append("demo", file);
+            formData.append("name", demoName);
+            formData.append("public", true);
+            formData.append("author_id", session.user.id);
+
+            setIsUploading(true);
+            await dispatch(postDemo(formData));
+            setAudioFile(null);
+            setName("");
+            setIsUploading(false);
+        } catch (error) {
+            console.log(error);
+        }
     };
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -74,7 +86,7 @@ const Demos = () => {
         await dispatch(deleteDemo(demo));
         closeModal();
     };
-    console.log(audioFile);
+    console.log(audioFile, name);
     return (
         <div className="demos-page">
             {isUploading ? (
