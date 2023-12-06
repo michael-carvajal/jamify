204bdd1e5443ae6a383843e152bf2657d762098e
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-12-05 20:19:20 -0500

    landing page main section done

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -21,14 +21,14 @@ export default function LandingPage() {
     return (
         <div className="landing-page overflow-hidden">
             <h1 className="text-xl md:text-3xl font-bold text-center">Welcome To Jamify</h1>
-            <div className=" ">
-                <h3>Top Hits</h3>
+            <div >
+                <h3 className="md:text-lg">Top Hits</h3>
                 <div className="top-hits-body my-5">
                     <img style={{ width: "30%" }} className="hidden md:block" id="top-hits-img" src="https://jamify-aa.s3.us-east-2.amazonaws.com/Artists/ABBA.jpg" alt="band"></img>
                     <div className="grid grid-cols-3  p-2">
-                        <div className="font-bold">Artist</div>
-                        <div className="font-bold">Title</div>
-                        <div className="font-bold">Rating</div>
+                        <div className="font-bold text-xs md:text-base">Artist</div>
+                        <div className="font-bold text-xs md:text-base">Title</div>
+                        <div className="font-bold text-xs md:text-base">Rating</div>
                         {topHits.map((song, index) => {
                             if (index > 3) {
                                 return null;
@@ -57,53 +57,49 @@ export default function LandingPage() {
                 </div>
 
             </div>
-            <div className="categories-published">
-
-                <div className="categories">
-                    <h3>Explore our Songsheet Catalog</h3>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>GENRE</th>
-                                <th>DECADE</th>
-                                <th>ARTISTS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {decades.map((decade, index) => {
-                                const dataKey = index + 1
-                                return (
-                                    <tr key={`decades-${index}`}>
-                                        <th>{Genres[dataKey].name}</th>
-                                        <th>{decade}</th>
-                                        <th>{Artists[dataKey]?.name || ""}</th>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+            <div className="flex flex-col md:flex-row gap-6 md:gap-2">
                 <div className="recently-published">
                     <h3>Recently Published</h3>
-                    <table>
-                        <thead>
-
-                        </thead>
-                        <tbody>
-                            {recentPublish.reverse().map((song, index) => {
-                                const artistName = Artists[song.artist_id].name
-                                return (
-                                    <tr key={`recently-published-${index}`}>
-                                        <th>{artistName}</th>
-                                        <th><NavLink to={`/songsheet-detail/${song.id}`} className="songsheet-link">{song.title}</NavLink></th>
-                                        <th>{Math.floor(Math.random() * 1000)} Likes</th>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
+                    <div className="grid grid-cols-3 p-2">
+                        <div>Artist</div>
+                        <div>Title</div>
+                        <div>Likes</div>
+                        {recentPublish.reverse().map((song, index) => {
+                            const artistName = Artists[song.artist_id].name;
+                            return (
+                                <React.Fragment key={`recently-published-${index}`}>
+                                    <div className="border-t p-2">{artistName}</div>
+                                    <div className="border-t p-2">
+                                        <NavLink to={`/songsheet-detail/${song.id}`} className="songsheet-link">
+                                            {song.title}
+                                        </NavLink>
+                                    </div>
+                                    <div className="border-t p-2">{Math.floor(Math.random() * 1000)} Likes</div>
+                                </React.Fragment>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="categories">
+                    <h3>Explore our Songsheet Catalog</h3>
+                    <div className="grid grid-cols-3 p-2">
+                        <div>GENRE</div>
+                        <div>DECADE</div>
+                        <div>ARTISTS</div>
+                        {decades.map((decade, index) => {
+                            const dataKey = index + 1;
+                            return (
+                                <React.Fragment key={`decades-${index}`}>
+                                    <div className="border-t p-2">{Genres[dataKey].name}</div>
+                                    <div className="border-t p-2">{decade}</div>
+                                    <div className="border-t p-2">{Artists[dataKey]?.name || ""}</div>
+                                </React.Fragment>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
