1898628856571b5992f4d59b75f252725ff4e9e7
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-12-13 21:20:33 -0500

    uploading sign size

--- react-app/src/components/Demos/index.js
+++ react-app/src/components/Demos/index.js
@@ -93,10 +93,10 @@ const Demos = () => {
                 <div className="loading-btn demo-form">
                     <button
                         style={{ cursor: 'wait', alignSelf: 'center', marginBottom: '30px' }}
-                        className="upload-btn"
+                        className="upload-btn w-12 md:w-20 h-12 md:h-20"
                         id="isUploading"
                     ></button>
-                    <p style={{ textAlign: 'center', color: 'rgb(58, 58, 58)', zIndex: '10', fontWeight: '500' }}>Uploading {audioFile?.name}</p>
+                    <p style={{ textAlign: 'center', color: 'rgb(58, 58, 58)', zIndex: '10', fontWeight: '500' }} className='text-xl md:text-2xl'>Uploading {audioFile?.name}</p>
                     <div className="custom-shape-divider-bottom-1689291612">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                             <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
